583c45a83cffbd36965f46ba884bc92fc2c7afd5
Flavio Avdulla <a.flavio4366@gmail.com>
2024-09-02 10:40:05 +0000

    code update

--- debugging/factorial_recursive.py
+++ debugging/factorial_recursive.py
@@ -2,20 +2,20 @@
 import sys
 
 def factorial(n):
-    """
-    Calculate the factorial of a given number.
+	"""
+	Calculate the factorial of a number using recursion.
 
-    Parameters:
-    n (int): The number to calculate the factorial for.
+	Parameters:
+	n (int): The number to calculate the factorial of.
 
-    Returns:
-    int: The factorial of the given number.
-    """
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n-1)
+	Returns:
+	int: The factorial of the number n.
+	"""
+	if n == 0:
+		return 1
+	else:
+		return n * factorial(n-1)
 
+# Get the number from command line arguments and calculate its factorial
 f = factorial(int(sys.argv[1]))
-
 print(f)
--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -3,4 +3,4 @@
 import sys
 
 for i in range(1, len(sys.argv)):
-    print(sys.argv[i])
+	print(sys.argv[i])
